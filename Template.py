3843628f50d5a31071d4e46c201fcb82d8b64f4f
MrFanning <45568916+MrFanning@users.noreply.github.com>
2018-12-11 17:12:35 -0700

    Add files via upload

---
+++ Template.py
@@ -0,0 +1,13 @@
+# Welcome, This is a template file for a python program
+
+# Type What Program Does
+print("This program ..." )
+
+# Input a variable
+a = int(input("Input a"))
+
+# Do calculation
+b = a**2 # finds a**2
+
+# Write solution
+print("", b) 
