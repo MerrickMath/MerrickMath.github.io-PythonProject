ae6613144c33ce065803ade747df80c169fae43b
MrFanning <45568916+MrFanning@users.noreply.github.com>
2018-12-12 12:11:39 -0700

    Add files via upload

---
+++ Square.py
@@ -0,0 +1,6 @@
+# This Is A Program That Finds Area Given Sidelength 
+print("This is a Program That Finds The Area Of A Square Given A Sidelength")
+a = input("input a sidelength: ")
+a = int(a)
+area = a**2
+print("Area: ", area) 
